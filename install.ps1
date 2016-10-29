c0b57a2a2f4ccccf990d4f77076ba93d8461d5a1
yabona <noahbailey9214201@gmail.com>
2016-10-28 23:38:29 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,14 @@
-if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts)) 
-{ New-Item -Type File -Path $PROFILE.CurrentUserAllHosts -Force }
-	$PsHome\Profile.ps1
+if $allusers {
+	if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts)) { 
+		New-Item -Type File -Path $PROFILE.CurrentUserAllHosts -Force 
+	}
+	Copy-Item -path '.\Microsoft.PowerShell_profile.ps1' -destination '$PsHome\Profile.ps1'
+} else {
+	if (!(test-path -path $profile)	{
+		new-item -type file -path $profile -force	
+	}	
+	Copy-Item -path '.\Microsoft.PowerShell_profile.ps1' -destination $env:USERPROFILE\Documents\WindowsPowershell\Microsoft.PowerShell_Profile.ps1
+}
 
-Copy-Item -path '.\Microsoft.PowerShell_profile.ps1' -destination '$PsHome\Profile.ps1'
+write-host "...Done!"
 pause
